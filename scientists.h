206db71a22ea18997a01162665b4f2a0184f6be0
Sandra Bender <sandrabender@orkuver22.lbhi.is>
2015-11-26 20:07:47 +0000

    Bætti við nokkrum föllum

--- scientists.h
+++ scientists.h
@@ -14,6 +14,16 @@ class Scientists {
     public:
         Person();
         ~Person();
+        void sortByName();
+        void sortByPerson();
+        void sortBySex();
+        void search();
+        void display();
+        void getFile();
+        void inputFile();
+        void checkIfExists();
+        void getAlignment(); //
+
         void insert(const string& name);
         friend ostream& operator <<(ostream& outs, const Scientists& p);
     private:
