ed2a70282eb2d576c48c9ed4f0a6d1efe77eab54
erling14 <erling14@ru.is>
2015-12-01 20:35:11 +0000

    .

--- database.cpp
+++ database.cpp
@@ -13,7 +13,7 @@ void database::readFile(vector<Scientist>& vec) // This function reads the file
 
   datas.open("/Users/sandrabender/hopur2/input.csv"); //Breyta path
   //datas.open("c:/temp/input.csv"); ABO
-
+    datas.open("C:/Users/FLX/Desktop/hopur2/input.csv");
   //datas.open("c:/temp/input.csv");
 
 
